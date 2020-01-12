c05251a21f790dc73e16d9f189d472f9ccbde57c
mrx <mrx003@gmail.com>
2020-01-12 19:41:43 -0300

    merda

---
+++ mrx escola.py
@@ -0,0 +1,80 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/ronyf/OneDrive/Área de Trabalho/scripts/tabuada.py ====
+digite um numero para ver a sua tabuada6
+==============================
+ 6.0 x 1 = 6.0
+ 6.0 x 2 = 12.0
+ 6.0 x 3 = 18.0
+ 6.0 x 4 = 24.0
+ 6.0 x 5 = 30.0
+ 6.0 x 6 = 36.0
+ 6.0 x 7 = 42.0
+ 6.0 x 8 = 48.0
+ 6.0 x 9 = 54.0
+ 6.0 x 10 = 60.0
+==============================
+>>> 
+==== RESTART: C:/Users/ronyf/OneDrive/Área de Trabalho/scripts/tabuada.py ====
+digite um numero para ver a sua tabuada:10
+Traceback (most recent call last):
+  File "C:/Users/ronyf/OneDrive/Área de Trabalho/scripts/tabuada.py", line 13, in <module>
+    print('{:---^30}'.format(l))
+ValueError: Invalid format specifier
+>>> 
+==== RESTART: C:/Users/ronyf/OneDrive/Área de Trabalho/scripts/tabuada.py ====
+digite um numero para ver a sua tabuada:10
+=============---==============
+ 10.0 x 1 = 10.0
+ 10.0 x 2 = 20.0
+ 10.0 x 3 = 30.0
+ 10.0 x 4 = 40.0
+ 10.0 x 5 = 50.0
+ 10.0 x 6 = 60.0
+ 10.0 x 7 = 70.0
+ 10.0 x 8 = 80.0
+ 10.0 x 9 = 90.0
+ 10.0 x 10 = 100.0
+=============---==============
+>>> 
+==== RESTART: C:/Users/ronyf/OneDrive/Área de Trabalho/scripts/tabuada.py ====
+digite um numero para ver a sua tabuada:7
+=============---==============
+
+Bem vindo a fila de ESCOLA!!!
+=============---==============
+ 7.0 x 1 = 7.0
+ 7.0 x 2 = 14.0
+ 7.0 x 3 = 21.0
+ 7.0 x 4 = 28.0
+ 7.0 x 5 = 35.0
+ 7.0 x 6 = 42.0
+ 7.0 x 7 = 49.0
+ 7.0 x 8 = 56.0
+ 7.0 x 9 = 63.0
+ 7.0 x 10 = 70.0
+=============---==============
+>>> 
+==== RESTART: C:/Users/ronyf/OneDrive/Área de Trabalho/scripts/tabuada.py ====
+digite um numero para ver a sua tabuada:40
+=============---==============
+        __  __ ____  __  __
+       |  \/  |  _ \ \ \/ /
+       | |\/| | |_) | \  / 
+       | |  | |  _ <  /  \ 
+       |_|  |_|_| \_\/_/\_\ 
+=============---==============
+ 40.0 x 1 = 40.0
+ 40.0 x 2 = 80.0
+ 40.0 x 3 = 120.0
+ 40.0 x 4 = 160.0
+ 40.0 x 5 = 200.0
+ 40.0 x 6 = 240.0
+ 40.0 x 7 = 280.0
+ 40.0 x 8 = 320.0
+ 40.0 x 9 = 360.0
+ 40.0 x 10 = 400.0
+=============---==============
+>>> 
+>>> 
